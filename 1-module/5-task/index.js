e0106a27d9ac0174378663f74d684433459cd414
Pasheku <martynenko_pasha@mail.ru>
2022-05-25 23:02:10 +0300

    1-module

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,13 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    return str.substring(str, maxlength) + "...";
+  }
+  if (str.length < maxlength) {
+    return str.substring(str, maxlength);
+  }
 }
+
+truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) ===
+  "Вот, что мне хотело…";
+
+truncate("Всем привет!", 20) === "Всем привет!";

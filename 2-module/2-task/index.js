36b61e18055a36a79967f14b28ec759948e0247d
Pasheku <martynenko_pasha@mail.ru>
2022-05-28 21:58:55 +0300

    2-module, 2-task

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,19 @@
 function isEmpty(obj) {
-  // ваш код...
+  let counter = 0;
+  for (let key in obj) {
+    counter++;
+  }
+  if (counter == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+let schedule = {};
+
+alert(isEmpty(schedule)); // true
+
+schedule["8:30"] = "подъём";
+
+alert(isEmpty(schedule)); // false
